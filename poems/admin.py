40b430026a0033561f7da7c48b0a72d4d96537a2
rkyzk <reikoyazaki@yahoo.com>
2023-08-30 13:09:24 +0000

    add list items and search fields for Poem model on admin panel

--- poems/admin.py
+++ poems/admin.py
@@ -1,3 +1,14 @@
-from django.contrib import admin
+"""This module holds a class to customize the admin panel."""
 
-# Register your models here.
+from django.contrib import admin
+from django.db import models
+from .models import Poem
+
+
+@admin.register(Poem)
+class PoemAdmin(admin.ModelAdmin):
+    """Customize list items of Poem model on admin panel."""
+    list_display = ('owner', 'published', 'published_at',
+                    'featured_flag')
+    search_fields = ('owner', 'featured_flag')
+    list_filter = ('owner',)
