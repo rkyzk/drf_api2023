f13878f76240e5eef9210b51b36ddb9434028e81
rkyzk <reikoyazaki@yahoo.com>
2023-08-30 14:43:12 +0000

    make LikeDetail view and wire it

--- likes/urls.py
+++ likes/urls.py
@@ -1,6 +1,9 @@
+"""This module holds URL patterns for the paths in the likes apps."""
+
 from django.urls import path
 from likes import views
 
 urlpatterns = [
     path('likes/', views.LikeList.as_view()),
+    path('likes/<int:pk>', views.LikeDetail.as_view()),
 ]
