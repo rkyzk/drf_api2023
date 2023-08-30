b47998c17883418afc62518022c6c4760e73208f
rkyzk <reikoyazaki@yahoo.com>
2023-08-30 14:25:51 +0000

    make list items and search fields for Comment model on admin panel

--- comments/admin.py
+++ comments/admin.py
@@ -1,3 +1,13 @@
-from django.contrib import admin
+"""This module holds a class to customize the admin panel."""
 
-# Register your models here.
+from django.contrib import admin
+from django.db import models
+from .models import Comment
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    """Customizes list, filter and search items on admin panel."""
+    list_display = ('owner', 'status', 'created_at')
+    search_fields = ('owner',)
+    list_filter = ('owner',)
