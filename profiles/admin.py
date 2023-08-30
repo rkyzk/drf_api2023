8910e27034a7289034ce859a28c67232e5e6ef2c
rkyzk <reikoyazaki@yahoo.com>
2023-08-30 12:57:00 +0000

    make list items & search fields for Profile model on admin panel

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,13 @@
-from django.contrib import admin
+"""This module holds a class to customize the admin panel."""
 
-# Register your models here.
+from django.contrib import admin
+from django.db import models
+from .models import Profile
+
+
+@admin.register(Profile)
+class PostAdmin(admin.ModelAdmin):
+    """Customize list items of Profile model on admin panel."""
+    list_display = ('owner', 'display_name', 'created_at')
+    search_fields = ('owner', 'display_name')
+    list_filter = ('owner', 'display_name')
