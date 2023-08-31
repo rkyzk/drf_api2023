6d9c54c4c2360dd1bd2471f5ae93c43862a593a3
rkyzk <reikoyazaki@yahoo.com>
2023-08-31 11:56:58 +0000

    add FollowerList view and wire it

---
+++ followers/urls.py
@@ -0,0 +1,8 @@
+"""This module holds URL patterns for the paths in the likes apps."""
+
+from django.urls import path
+from followers import views
+
+urlpatterns = [
+    path('followers/', views.FollowerList.as_view()),
+]
--- followers/views.py
+++ followers/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+"""This module holds views for listing/creating Follower objects
+and for deleting Follower objects."""
 
-# Create your views here.
+from rest_framework import generics, permissions
+from drf_api.permissions import IsOwnerOrReadOnly
+from .models import Follower
+from .serializers import FollowerSerializer
+
+
+class FollowerList(generics.ListCreateAPIView):
+    """
+    List all followers, i.e. all instances of a user
+    following another user'.
+    Create a follower, i.e. follow a user if logged in.
+    Perform_create: associate the current logged in user with a follower.
+    """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Follower.objects.all()
+    serializer_class = FollowerSerializer
+
+    def perform_create(self, serializer):
+        """
+        When an object is created
+        Set the current user as the owner.
+        """
+        serializer.save(owner=self.request.user)
