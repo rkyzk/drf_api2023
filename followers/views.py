69aa2fee4463d0e9ff6cad2b7a995cbadb2f3503
rkyzk <reikoyazaki@yahoo.com>
2023-08-31 11:58:23 +0000

    add FollowerDetail view and wire it

--- followers/views.py
+++ followers/views.py
@@ -24,3 +24,14 @@ class FollowerList(generics.ListCreateAPIView):
         Set the current user as the owner.
         """
         serializer.save(owner=self.request.user)
+
+
+class FollowerDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieve a follower.
+    No Update view, as we either follow or unfollow users
+    Destroy a follower, i.e. unfollow someone if owner
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Follower.objects.all()
+    serializer_class = FollowerSerializer
