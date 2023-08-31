fbe686011792c820c2dea3d87a30c0be40372f45
rkyzk <reikoyazaki@yahoo.com>
2023-08-31 12:23:59 +0000

    install drf simplejwt, add logout_route method and wire it

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -29,6 +29,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+REST_USE_JWT = True
+JWT_AUTH_SECURE = True
+JWT_AUTH_COOKIE = 'my-app-auth'
+JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
+JWT_AUTH_SAMESITE = 'None'
+
 REST_AUTH_SERIALIZERS = {
     'USER_DETAILS_SERIALIZER':
         'drf_api.serializers.CurrentUserSerializer'
