e1b80e1bb5e9dc53d183ae83be80999ae5fb046c
rkyzk <reikoyazaki@yahoo.com>
2023-08-31 11:59:08 +0000

    add list items and search fields for Follower model on admin panel

--- followers/admin.py
+++ followers/admin.py
@@ -1,3 +1,13 @@
-from django.contrib import admin
+"""This module holds a class to customize the admin panel."""
 
-# Register your models here.
+from django.contrib import admin
+from django.db import models
+from .models import Follower
+
+
+@admin.register(Follower)
+class FollowerAdmin(admin.ModelAdmin):
+    """Add list, search, filter items on the admin panel."""
+    list_display = ['owner', 'followed', 'created_at']
+    search_fields = ['owner', 'followed']
+    list_filter = ['owner', 'followed']
