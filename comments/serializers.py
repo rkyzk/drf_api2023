56dbe7f7a19c915115e88be13983501d0da7f4fd
rkyzk <reikoyazaki@yahoo.com>
2023-09-03 02:26:12 +0000

    display 'XX days ago' for comments if made in the previous 7 days

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,13 +1,14 @@
-"""This module holds serailizer class for Comment model."""
 
+from django.contrib.humanize.templatetags.humanize import naturaltime
 from rest_framework import serializers
 from .models import Comment
+from datetime import datetime, timedelta
 
 
 class CommentSerializer(serializers.ModelSerializer):
     """
     Serializer for the Comment model
-    Adds 5 extra fields when returning a list of Comment instances
+    Adds ??    extra fields when returning a list of Comment instances
     """
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
@@ -15,10 +16,22 @@ class CommentSerializer(serializers.ModelSerializer):
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
     profile_name = serializers.ReadOnlyField(
                        source='owner.profile.display_name')
+    created_at = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+
+    def get_created_at(self, obj):
+        if obj.created_at.date() > datetime.now().date() - timedelta(days=7):
+            return naturaltime(obj.created_at)
+        return obj.created_at.strftime("%d %b %Y")
+
+    def get_updated_at(self, obj):
+        if obj.updated_at.date() > datetime.now().date() - timedelta(days=7):
+            return naturaltime(obj.updated_at)
+        return obj.updated_at.strftime("%d %b %Y")
 
     class Meta:
         model = Comment
