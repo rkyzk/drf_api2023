ab75afa186f1c69026c9703052af8221924d7d09
rkyzk <reikoyazaki@yahoo.com>
2023-09-08 06:03:08 +0000

    add CORS_ALLOWED_ORIGIN

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -71,8 +71,7 @@ ALLOWED_HOSTS = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    'https://3000-rkyzk-poetry2023-731m269odww.ws-us104.gitpod.io',
-    'http://192.168.1.23:3000',
+    'https://your-poetry-2023-094a4af35547.herokuapp.com',
 ]
 
 # Application definition
