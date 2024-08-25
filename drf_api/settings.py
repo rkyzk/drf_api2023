35639bdd35a536b3a5e69dce12658489123afd86
rkyzk <reikoyazaki@yahoo.com>
2024-08-25 11:40:40 +0000

    change db setting

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -157,15 +157,15 @@ WSGI_APPLICATION = 'drf_api.wsgi.application'
 #         }
 #     }
 # else:
-DATABASE_URL = os.getenv('DATABASE_URL')
+#DATABASE_URL = os.getenv('DATABASE_URL')
 DATABASES = {
-    'default': dj_database_url.config(default=DATABASE_URL),
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL")),
 }
-DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
-DATABASES['default']['NAME'] = os.getenv('DATABASE_NAME')
-DATABASES['default']['USER'] = os.getenv('DATABASE_USER')
-DATABASES['default']['PASSWORD'] = os.getenv('DATABASE_PASSWORD')
-DATABASES['default']['HOST'] = os.getenv('DATABASE_HOST')
+# DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
+# DATABASES['default']['NAME'] = os.getenv('DATABASE_NAME')
+# DATABASES['default']['USER'] = os.getenv('DATABASE_USER')
+# DATABASES['default']['PASSWORD'] = os.getenv('DATABASE_PASSWORD')
+# DATABASES['default']['HOST'] = os.getenv('DATABASE_HOST')
 
 
 
