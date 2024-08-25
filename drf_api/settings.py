2b7c2df2db182f17e25825dfcc083b4858a93a48
rkyzk <reikoyazaki@yahoo.com>
2024-08-25 02:44:44 +0000

    change database setting

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -150,18 +150,18 @@ WSGI_APPLICATION = 'drf_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if 'DEV' in os.environ:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
-    DATABASE_URL = os.getenv('DATABASE_URL')
-    DATABASES = {
-        'default': dj_database_url.config(),
-    }
+# if 'DEV' in os.environ:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
+# else:
+DATABASE_URL = os.getenv('DATABASE_URL')
+DATABASES = {
+    'default': dj_database_url.config(),
+}
 
 
 
