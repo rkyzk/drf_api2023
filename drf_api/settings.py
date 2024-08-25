72c79366ed7e7ae85155998e0d259a8d9436ff3f
rkyzk <reikoyazaki@yahoo.com>
2024-08-25 01:00:05 +0000

    correct the way secret key is obtained in settings.py

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -60,7 +60,7 @@ REST_AUTH_SERIALIZERS = {
 }
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
