101b81674e6ee9a26a8ba3f1fdd0ed16feb6466b
rkyzk <reikoyazaki@yahoo.com>
2024-08-25 00:54:16 +0000

    change database setting in settings.py

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -157,8 +157,9 @@ WSGI_APPLICATION = 'drf_api.wsgi.application'
 #         }
 #     }
 # else:
+DATABASE_URL = os.getenv('DATABASE_URL')
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': dj_database_url.config(),
 }
 
 
