59d156f45b630ef5a7d3a9475a903b7ab2710f5b
rkyzk <reikoyazaki@yahoo.com>
2024-08-25 04:07:05 +0000

    add databases setting

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -163,7 +163,11 @@ DATABASES = {
     'default': dj_database_url.config(default=DATABASE_URL),
 }
 DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql'
-DATABASES['default']['NAME'] = 'bqazouxg'
+DATABASES['default']['NAME'] = os.getenv('DATABASE_NAME')
+DATABASES['default']['USER'] = os.getenv('DATABASE_USER')
+DATABASES['default']['PASSWORD'] = os.getenv('DATABASE_PASSWORD')
+DATABASES['default']['HOST'] = os.getenv('DATABASE_HOST')
+DATABASES['default']['PORT'] = '5432'
 
 
 
